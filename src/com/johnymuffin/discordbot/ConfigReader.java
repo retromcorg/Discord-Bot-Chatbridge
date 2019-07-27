85000c73c1bdb48e4d74fad903e44b2ac79547c5
Johny Muffin <WHd9PmmTIyL3NfyjzYA2>
2019-07-27 12:25:14 +1000

    Custom messages, presence is not not a option :(

--- src/com/johnymuffin/discordbot/ConfigReader.java
+++ src/com/johnymuffin/discordbot/ConfigReader.java
@@ -6,11 +6,26 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.player.PlayerJoinEvent;
+
 class ConfigReader {
 	// New Config
-	private static String token = "";
+	
+	//discord.Discord().DiscordSendToChannel(gameBridge,
+	//"**" + ((PlayerJoinEvent) event).getPlayer().getName() + "** Has Joined The Game ["
+	//+ Bukkit.getServer().getOnlinePlayers().length + "/"
+	//+ Bukkit.getServer().getMaxPlayers() + "]");
+
+	private static String discordChatMessage = "&f[&6Discord&f]&7 %messageAuthor%: %message%";
+	private static String gameChatMessage = "**%messageAuthor%**: %message%";
+	private static String joinMessage = "**%username%** Has Joined THe Game [%onlineCount%/%maxCount%]";
+	private static String quitMessage = "**%username%** Has Quit THe Game [%onlineCount%/%maxCount%]";
+	
 	private static String serverName = "";
 	private static String channelID = "";
+	private static Boolean PrensencePlayercount = Boolean.TRUE;
+	private static Boolean OnlineCommand = Boolean.TRUE;
 
 	ConfigReader(DiscordBot instance) {
 		Properties prop = new Properties();
@@ -26,6 +41,23 @@ class ConfigReader {
 		// New Config
 		serverName = prop.getProperty("serverName", serverName);
 		channelID = prop.getProperty("channelID", channelID);
+		PrensencePlayercount = Boolean.valueOf(Boolean.parseBoolean(prop.getProperty("PrensencePlayercount", "" + PrensencePlayercount)));
+		OnlineCommand = Boolean.valueOf(Boolean.parseBoolean(prop.getProperty("OnlineCommand", "" + OnlineCommand)));
+		//Join Custom Message
+		joinMessage = prop.getProperty("joinMessage", joinMessage);
+		prop.setProperty("joinMessage", joinMessage);
+		//Quit Custom Message
+		quitMessage = prop.getProperty("quitMessage", quitMessage);
+		prop.setProperty("quitMessage", quitMessage);
+		//Discord Chat Message
+		discordChatMessage = prop.getProperty("discordChatMessage", discordChatMessage);
+		prop.setProperty("discordChatMessage", discordChatMessage);
+		//Game Chat Message
+		gameChatMessage = prop.getProperty("gameChatMessage", gameChatMessage);
+		prop.setProperty("gameChatMessage", gameChatMessage);
+		
+		prop.setProperty("OnlineCommand", "" + OnlineCommand);
+		prop.setProperty("PrensencePlayercount", "" + PrensencePlayercount);
 		prop.setProperty("channelID", channelID);
 		prop.setProperty("serverName", serverName);
 
@@ -43,6 +75,28 @@ class ConfigReader {
 	
 	String getChannel() {
 		return channelID;
+	}
+	
+	String getJoinMessage() {
+		return joinMessage;
+	}
+	String getQuitMessage() {
+		return quitMessage;
+	}
+	String getDiscordChatMessage() {
+		return discordChatMessage;
+	}
+	String getGameChatMessage() {
+		return gameChatMessage;
+	}
+	
+	Boolean getPrensencePlayercount() {
+		return PrensencePlayercount;
+		
+	}
+	Boolean getOnlineCommand() {
+		return OnlineCommand;
+		
 	}
 
 	void onDisable() {
