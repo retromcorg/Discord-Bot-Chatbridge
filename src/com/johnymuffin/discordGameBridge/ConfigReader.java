e4d8d92fccf8d129692bd63bb70568278d476891
Ashleyz4 <epicman56@yahoo.com>
2019-11-04 05:47:32 -0600

    Version 1.5
    Better implementation to the ConfigReader
    New "separateChat" config variable - Splits chat based on last user to talk
    New "discordStartMessage" config variable - Upon start, shows this message if "announceStartStop" is "true"
    New "showTimestamp" config var along with "useInGameTime" and "timestampMessage"
    Better & working replacement in a message for sending to Discord

--- src/com/johnymuffin/discordGameBridge/ConfigReader.java
+++ src/com/johnymuffin/discordGameBridge/ConfigReader.java
@@ -1,103 +1,121 @@
 package com.johnymuffin.discordGameBridge;
 
+import javax.annotation.Nonnull;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 class ConfigReader {
-    // New Config
-
-    //discord.Discord().DiscordSendToChannel(gameBridge,
-    //"**" + ((PlayerJoinEvent) event).getPlayer().getName() + "** Has Joined The Game ["
-    //+ Bukkit.getServer().getOnlinePlayers().length + "/"
-    //+ Bukkit.getServer().getMaxPlayers() + "]");
-
+    private String discordStartMessage = "**Server Has Started**";
     private String discordChatMessage = "&f[&6Discord&f]&7 %messageAuthor%: %message%";
     private String gameChatMessage = "**%messageAuthor%**: %message%";
-    private String joinMessage = "**%username%** Has Joined THe Game [%onlineCount%/%maxCount%]";
-    private String quitMessage = "**%username%** Has Quit THe Game [%onlineCount%/%maxCount%]";
-
+    private String joinMessage = "**%username%** Has Joined The Game [%onlineCount%/%maxCount%]";
+    private String quitMessage = "**%username%** Has Quit The Game [%onlineCount%/%maxCount%]";
+    private String timestampMessage = "**[%world%( - )%h%:%m%:%s%:%S% %ampm% %z%]** ";
     private String serverName = "";
     private String channelID = "";
-    private Boolean PrensencePlayercount = true;
-    private Boolean OnlineCommand = true;
-    private boolean announceStartStop = true;
 
-    ConfigReader(DiscordBot instance) {
+    private boolean presencePlayercount = true;
+    private boolean onlineCommand = true;
+    private boolean announceStartStop = true;
+    private boolean showTimestamp = true;
+    private boolean useInGameTime = true;
+    private boolean seperateChat = true;
+
+    ConfigReader() {
+        this.reload();
+    }
+
+    private int reloadTimes = 0;
+    void reload() {
         Properties prop = new Properties();
+
+        DiscordBot instance = DiscordBot.getInstance();
+        if(reloadTimes > 3) {
+            reloadTimes = 0;
+            instance.logger.severe("[DiscordBot]: Failed to create/read properties file after 3 times at " + instance.config.getAbsolutePath());
+            return;
+        }
 
         try {
             prop.load(new FileInputStream(instance.config));
         } catch (IOException var4) {
-            instance.logger.warning("[RetroBot] No properties found! Making new file...");
-            var4.printStackTrace();
+            instance.logger.warning("[DiscordBot]: No properties file found! Attempting to create...");
+            if(!instance.config.exists()) {
+                try {
+                    boolean b = instance.config.createNewFile();
+                    if(!b) {
+                        instance.logger.warning("[DiscordBot]: Failed to create file at " + instance.config.getAbsolutePath());
+                    } else {
+                        reloadTimes++;
+                        instance.logger.fine("[DiscordBot]: Successfully created new config, trying again...");
+                        reload();
+                    }
+                    return;
+                } catch(IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            } else {
+                if(!instance.config.canRead()) {
+                    instance.logger.severe("[DiscordBot]: Can't read from config file at " + instance.config.getAbsolutePath());
+                    return;
+                }
+            }
         }
-        // New Config
-        serverName = prop.getProperty("serverName", serverName);
-        channelID = prop.getProperty("channelID", channelID);
-        PrensencePlayercount = Boolean.parseBoolean(prop.getProperty("PrensencePlayercount", "" + PrensencePlayercount));
-        OnlineCommand = Boolean.parseBoolean(prop.getProperty("OnlineCommand", "" + OnlineCommand));
-        announceStartStop = Boolean.parseBoolean(prop.getProperty("announceStartStop", "" + announceStartStop));
-        //Join Custom Message
-        joinMessage = prop.getProperty("joinMessage", joinMessage);
-        prop.setProperty("joinMessage", joinMessage);
-        //Quit Custom Message
-        quitMessage = prop.getProperty("quitMessage", quitMessage);
-        prop.setProperty("quitMessage", quitMessage);
-        //Discord Chat Message
-        discordChatMessage = prop.getProperty("discordChatMessage", discordChatMessage);
-        prop.setProperty("discordChatMessage", discordChatMessage);
-        //Game Chat Message
-        gameChatMessage = prop.getProperty("gameChatMessage", gameChatMessage);
-        prop.setProperty("gameChatMessage", gameChatMessage);
-        //Announce Server Start/Stop
 
-        prop.setProperty("OnlineCommand", "" + OnlineCommand);
-        prop.setProperty("PrensencePlayercount", "" + PrensencePlayercount);
-        prop.setProperty("channelID", channelID);
-        prop.setProperty("serverName", serverName);
-        prop.setProperty("announceStartStop", ""+announceStartStop);
+        reloadTimes = 0;
+
+        seperateChat = getAndSet_B(prop, "separateChat", seperateChat);
+        showTimestamp = getAndSet_B(prop, "showTimestamp", showTimestamp);
+        useInGameTime = getAndSet_B(prop, "useInGameTime", useInGameTime);
+        presencePlayercount = getAndSet_B(prop, "presencePlayercount", presencePlayercount);
+        onlineCommand = getAndSet_B(prop, "onlineCommand", onlineCommand);
+        announceStartStop = getAndSet_B(prop, "announceStartStop", announceStartStop);
+
+        serverName = getAndSet_S(prop, "serverName", serverName);
+        channelID = getAndSet_S(prop, "channelID", channelID);
+        joinMessage = getAndSet_S(prop, "joinMessage", joinMessage);
+        quitMessage = getAndSet_S(prop, "quitMessage", quitMessage);
+        discordStartMessage = getAndSet_S(prop, "discordStartMessage", discordStartMessage);
+        discordChatMessage = getAndSet_S(prop, "discordChatMessage", discordChatMessage);
+        gameChatMessage = getAndSet_S(prop, "gameChatMessage", gameChatMessage);
+        timestampMessage = getAndSet_S(prop, "timestampMessage", timestampMessage);
 
         try {
             prop.store(new FileOutputStream(instance.config), "Properties for DiscordBot");
-        } catch (Exception var3) {
-            instance.logger.severe("Failed to save properties for RetroBot!");
-            var3.printStackTrace();
+        } catch (Exception e) {
+            instance.logger.severe("[DiscordBot]: Failed to save properties!");
+            e.printStackTrace();
         }
     }
 
-    String getServerName() {
-        return this.serverName;
+    private String getAndSet_S(@Nonnull Properties prop, @Nonnull String get, @Nonnull Object Default) {
+        String s = prop.getProperty(get, String.valueOf(Default));
+        prop.setProperty(get, String.valueOf(s));
+        return s;
+    }
+    private boolean getAndSet_B(@Nonnull Properties prop, @Nonnull String get, @Nonnull Object Default) {
+        Boolean s = Boolean.parseBoolean(prop.getProperty(get, String.valueOf(Default)));
+        prop.setProperty(get, String.valueOf(s));
+        return s;
     }
 
-    String getChannel() {
-        return this.channelID;
-    }
+    String getServerName() { return this.serverName; }
+    String getChannel() { return this.channelID; }
+    String getJoinMessage() { return this.joinMessage; }
+    String getQuitMessage() { return this.quitMessage; }
+    String getDiscordStartMessage() { return discordStartMessage; }
+    String getDiscordChatMessage() { return this.discordChatMessage; }
+    String getGameChatMessage() { return this.gameChatMessage; }
+    String getTimestampMessage() { return timestampMessage; }
 
-    String getJoinMessage() {
-        return this.joinMessage;
-    }
-    String getQuitMessage() {
-        return this.quitMessage;
-    }
-    String getDiscordChatMessage() {
-        return this.discordChatMessage;
-    }
-    String getGameChatMessage() {
-        return this.gameChatMessage;
-    }
+    boolean getPresencePlayercount() { return this.presencePlayercount; }
+    boolean getOnlineCommand() { return this.onlineCommand; }
 
-    Boolean getPrensencePlayercount() {
-        return this.PrensencePlayercount;
-
-    }
-    Boolean getOnlineCommand() {
-        return this.OnlineCommand;
-    }
-
-    boolean canAnnounceStartStop() {
-        return announceStartStop;
-    }
+    boolean canUseInGameTime() { return useInGameTime; }
+    boolean canShowTimestamp() { return showTimestamp; }
+    boolean canSeparateChat() { return seperateChat; }
+    boolean canAnnounceStartStop() { return announceStartStop; }
 }
--- src/com/johnymuffin/discordGameBridge/GamePlayerEvents.java
+++ src/com/johnymuffin/discordGameBridge/GamePlayerEvents.java
@@ -2,6 +2,7 @@ package com.johnymuffin.discordGameBridge;
 
 import com.johnymuffin.discordcore.DiscordCore;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
@@ -9,6 +10,14 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import ru.tehkode.permissions.PermissionManager;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
 
+import javax.annotation.Nullable;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GamePlayerEvents extends PlayerListener {
@@ -28,11 +37,13 @@ public class GamePlayerEvents extends PlayerListener {
     @Override
     public void onPlayerJoin(PlayerJoinEvent event) {
         String message = config.getJoinMessage();
+        trySeperate(event.getPlayer().getName());
         message = message.replaceAll("%username%", event.getPlayer().getName());
         message = message.replaceAll("%onlineCount%",
                 Integer.toString(Bukkit.getServer().getOnlinePlayers().length));
         message = message.replaceAll("%maxCount%", Integer.toString(Bukkit.getServer().getMaxPlayers()));
-        dbc.Discord().DiscordSendToChannel(config.getChannel(), message);
+        trySeperate(event.getPlayer().getName());
+        dbc.Discord().DiscordSendToChannel(config.getChannel(), grabTimestamp(event.getPlayer().getWorld()) + message);
 
     }
 
@@ -43,17 +54,20 @@ public class GamePlayerEvents extends PlayerListener {
         message = message.replaceAll("%onlineCount%",
                 Integer.toString(Bukkit.getServer().getOnlinePlayers().length - 1));
         message = message.replaceAll("%maxCount%", Integer.toString(Bukkit.getServer().getMaxPlayers()));
-        dbc.Discord().DiscordSendToChannel(config.getChannel(), message);
+        trySeperate(event.getPlayer().getName());
+        dbc.Discord().DiscordSendToChannel(config.getChannel(), grabTimestamp(event.getPlayer().getWorld()) + message);
     }
 
     private String replaceMsg(String trueMsg) {
         trueMsg = trueMsg.replaceAll(Pattern.quote("@"), " ");
         trueMsg = trueMsg.replaceAll(Pattern.quote("@everyone"), " ");
         trueMsg = trueMsg.replaceAll(Pattern.quote("@here"), " ");
-        trueMsg = trueMsg.replaceAll(Pattern.quote("*"), "\\*");
-        trueMsg = trueMsg.replaceAll(Pattern.quote("_"), "\\_");
-        trueMsg = trueMsg.replaceAll(Pattern.quote("|"), "\\|");
-        trueMsg = trueMsg.replaceAll(Pattern.quote("~"), "\\~");
+        // V this is stupid
+        trueMsg = trueMsg.replaceAll("\\*", "\\\\\\\\*");
+        trueMsg = trueMsg.replaceAll("_", "\\\\\\\\_");
+        trueMsg = trueMsg.replaceAll("\\|", "\\\\\\\\|");
+        trueMsg = trueMsg.replaceAll("~", "\\\\\\\\~");
+        trueMsg = trueMsg.replaceAll(":", "\\\\\\\\:");
         return trueMsg;
     }
 
@@ -72,9 +86,86 @@ public class GamePlayerEvents extends PlayerListener {
             message = message.replaceAll("%group%", groupName);
             message = message.replaceAll("%messageAuthor%", event.getPlayer().getName());
             message = message.replaceAll("%message%", trueMsg);
-            dbc.Discord().DiscordSendToChannel(config.getChannel(), message);
+            trySeperate(event.getPlayer().getName());
+            dbc.Discord().DiscordSendToChannel(config.getChannel(), grabTimestamp(event.getPlayer().getWorld()) + message);
         }
     }
 
-
+    private String lastName = "";
+    //private LocalDateTime lastTime;
+    private void trySeperate(String playerName) {
+        if(config.canSeparateChat()) {
+            if(lastName.equals(playerName))
+                return;
+            dbc.Discord().DiscordSendToChannel(config.getChannel(), "​" /* zero width space */);
+            lastName = playerName;
+        }
+    }
+    private String grabTimestamp(@Nullable World world) {
+        if(config.canShowTimestamp()) {
+            LocalDateTime time = LocalDateTime.now();
+            String timeMessage = config.getTimestampMessage();
+            String minutes;
+            String seconds;
+            String milli;
+            String ampm = "PM";
+            if(config.canUseInGameTime()) {
+                long tHours;
+                long tMinutes;
+                long tSeconds;
+                long tMilli;
+                if(world == null)
+                    return "";
+                long worldTime = world.getTime();
+                tHours = worldTime / 1000 + 6;
+                tMinutes = (worldTime % 1000) * 60 / 1000;
+                tSeconds = (worldTime % 1000) * 60 / 1000 / 1000;
+                tMilli = (worldTime % 1000) * 60 / 1000 / 1000 / 1000;
+                if (tHours >= 12)
+                    tHours -= 12; ampm = "AM";
+                if (tHours == 0) tHours = 12;
+                timeMessage = timeMessage.replaceAll("%H%", tHours+"");
+                timeMessage = timeMessage.replaceAll("%h%", (tHours%12)+"");
+                String mm = "0" + tMinutes;
+                minutes = mm.substring(mm.length() - 2);
+                seconds = String.valueOf(tSeconds);
+                timeMessage = timeMessage.replaceAll("%S%", tMilli+"");
+                timeMessage = timeMessage.replaceAll("%z%", "");
+            } else {
+                timeMessage = timeMessage.replaceAll("%h%", String.valueOf(time.getHour())); // // 12hr format
+                timeMessage = timeMessage.replaceAll("%H%", time.format(DateTimeFormatter.ofPattern("H"))); // 24hr format
+                minutes = String.valueOf(time.getMinute());
+                if(minutes.length() < 2)
+                    minutes = "0"+minutes;
+                seconds = String.valueOf(time.getSecond());
+                timeMessage = timeMessage.replaceAll("%S%", time.format(DateTimeFormatter.ofPattern("S"))); // milliseconds
+                Calendar now = Calendar.getInstance();
+                timeMessage = timeMessage.replace("%z%", "GMT"+now.getTimeZone().getRawOffset());
+                ampm = time.format(DateTimeFormatter.ofPattern("a")).toUpperCase(); // AM/PM
+            }
+            timeMessage = timeMessage.replaceAll("%m%", minutes+"");
+            timeMessage = timeMessage.replaceAll("%s%", seconds+"");
+            timeMessage = timeMessage.replaceAll("%day%", time.getDayOfWeek().name());
+            timeMessage = timeMessage.replaceAll("%year%", time.getYear()+"");
+            timeMessage = timeMessage.replaceAll("%ampm%", ampm);
+            if(timeMessage.contains("%world%")) {
+                Pattern pattern = Pattern.compile("%world%(\\((.*)\\))");
+                Matcher matcher = pattern.matcher(timeMessage);
+                while(matcher.find()) {
+                    // there's likely an easier way to do this
+                    // but I'm too lazy to figure it out right now. - Ash
+                    if(world != null && config.canUseInGameTime())
+                        timeMessage = timeMessage.replace("%world%"+matcher.group(1), "%world%"+matcher.group(2));
+                    else
+                        timeMessage = timeMessage.replace("%world%"+matcher.group(1), "");
+                }
+                if(world != null && config.canUseInGameTime())
+                    timeMessage = timeMessage.replaceAll("%world%", world.getName());
+                else
+                    timeMessage = timeMessage.replaceAll("%world%", "");
+            }
+            return timeMessage;
+        }
+        return "";
+    }
 }
